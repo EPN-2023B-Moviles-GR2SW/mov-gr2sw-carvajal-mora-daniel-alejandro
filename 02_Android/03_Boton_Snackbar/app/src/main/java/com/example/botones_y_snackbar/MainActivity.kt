4307dd7f5ad120b980dface0d4d3f653b706f1a8
Daniel <danielcarva2000@hotmail.com>
2023-12-06 22:46:41 -0500

    Ciclo Vida Aplicacion y SnackBar

---
+++ 02_Android/03_Boton_Snackbar/app/src/main/java/com/example/botones_y_snackbar/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.botones_y_snackbar
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener{
+            irActividad(ACicloVida::class.java)
+        }
+    }
+
+    // Funcion
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+}
+
---
+++ 02_Android/03_Boton_Snackbar/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Botones_y_Snackbar"
+include(":app")
+ 
