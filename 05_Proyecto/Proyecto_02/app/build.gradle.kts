ec16f1473a555a57829590628652160da7efd5b3
Daniel <danielcarva2000@hotmail.com>
2024-02-19 13:41:50 -0500

    Creacion Carpetas Examen 2 y Proyecto 2

--- 05_Proyecto/Proyecto_02/app/build.gradle.kts
+++ 05_Proyecto/Proyecto_02/app/build.gradle.kts
@@ -1,18 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.firebase.appdistribution")
-    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.examen_segundo_bimestre"
-    compileSdk = 34
+    namespace = "com.example.proyecto_segundo_bimestre"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.examen_segundo_bimestre"
+        applicationId = "com.example.proyecto_segundo_bimestre"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -53,17 +51,6 @@ android {
 
 dependencies {
 
-
-    // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:30.1.0"))
-    // Libreria de Autenticacion
-    implementation("com.google.android.gms:play-services-auth:20.0.1")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    // UI libreria auth
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
-    // Firestore
-    implementation("com.google.firebase:firebase-firestore-ktx")
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -72,9 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
